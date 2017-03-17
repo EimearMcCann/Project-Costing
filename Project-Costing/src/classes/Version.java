4d484bf556749de3cc442a08f82b2190a0a149f6
Marinus <marinus@home>
2017-03-17 20:23:49 +0000

    Added new instance fields and setters and getters for them to the Version class.

--- Project-Costing/src/classes/Version.java
+++ Project-Costing/src/classes/Version.java
@@ -10,11 +10,21 @@ public class Version {
     // Instance Fields
     private final int number;
     private static int nextNumber = 1;
+    private String name;
+    private Date startDate;
+    private Date endDate;
+    private double cost;
+    private double duration;
     private ArrayList<Task> tasks;
 
     // Constructors
-    public Version() {
+    public Version(String name) {
         number = nextNumber++;
+        this.name = name;
+        startDate = new Date();
+        duration = 0;
+        cost = 0;
+        endDate = null;
         tasks = new ArrayList();
     }
 
@@ -26,6 +36,43 @@ public class Version {
     public int getNumber() {
         return number;
     }
+
+    public static void setNextNumber(int nextNumber) {
+        Version.nextNumber = nextNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+    
 
     public ArrayList<Task> getTasks() {
         return tasks;
@@ -55,5 +102,11 @@ public class Version {
             removed = tasks.remove(temp);
         }
         return removed;
+    }
+    
+    public void printTasks() {
+        for (Task t : tasks) {
+            System.out.println(t.toString());
+        }
     }
 }
