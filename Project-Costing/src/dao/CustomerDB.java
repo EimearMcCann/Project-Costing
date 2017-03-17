8d4881328bf2e5f5088cade2637aa83bd833ae57
Marinus <marinus@home>
2017-03-17 22:47:31 +0000

    Created class DBHandler and subclass CustomerDB.

---
+++ Project-Costing/src/dao/CustomerDB.java
@@ -0,0 +1,125 @@
+package dao;
+
+import classes.Customer;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author Marinus Toman Date: 17-Mar-2017
+ */
+public class CustomerDB extends DBHandler {
+    // Instance Fields
+
+    // Constructors
+    public CustomerDB(String user, String pass) {
+        super(user, pass);
+    }
+
+    // Methods
+    public Customer read(int id) {
+        Customer c = null;
+        // if id exists in table,
+        // then create query and create customer
+        if (id <= getCount()) {
+            try {
+                // if connection to db is closed, open it
+                if (conn.isClosed()) {
+                    openConnection();
+                }
+                String query;
+                query = makeSelectCustomer(id);
+                ResultSet rs = stmt.executeQuery(query);
+                Customer.setNextID(id);
+                rs.next();
+                // create customer based on results
+                c = new Customer(rs.getString(1), rs.getString(2),
+                        rs.getString(3), rs.getString(4),
+                        rs.getString(5), rs.getString(6));
+            } catch (SQLException e) {
+                System.out.println("Problem with SQL.\n" + e.getMessage());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                closeConnection();
+            }
+        }
+        return c;
+    }
+
+    public void write(Customer c) {
+        if (c != null) {
+            try {
+                // if connection to db is closed, open it
+                if (conn.isClosed()) {
+                    openConnection();
+                }
+                // create query to insert customer info
+                String query = makeInsertQuery(c.getName(), 
+                        c.getAddress().getStreet(), c.getAddress().getTown(), 
+                        c.getAddress().getCounty(), c.getPhone(), c.getEmail());
+                // execute query
+                stmt.executeUpdate(query);
+                System.out.println("success...written to db");
+            } catch (NullPointerException npe) {
+                System.out.println(npe.getMessage());
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                closeConnection();
+            }
+        }
+    }
+
+    private static String makeSelectCustomer(int id) {
+        String select = "SELECT Name, Street, Town, County, PhoneNo, Email "
+                + "FROM project_planner.customer WHERE ID = " + id + ";";
+
+        return select;
+    }
+
+    private static String makeInsertQuery(String name, String street, String town, String county, String phone, String email) {
+        String insert = "INSERT INTO project_planner.customer (Name, Street, Town, County, PhoneNo, Email) "
+                + "VALUES ('" + name + "', '" + street + "', '" + town + "', '" + county + "', '" + phone + "', '" + email + "');";
+
+        return insert;
+    }
+
+    private void createCustomerTable() {
+        //create table
+        String createTable = "CREATE TABLE IF NOT EXISTS customer "
+                + "(CustomerID INTEGER auto_increment not NULL, "
+                + " Name VARCHAR(255), "
+                + " Street VARCHAR(255), "
+                + " Town VARCHAR(255), "
+                + " County VARCHAR(255), "
+                + " PhoneNo VARCHAR(255),"
+                + " Email VARCHAR(255),"
+                + " PRIMARY KEY ( CustomerID ));";
+        //create customer table
+        try {
+            stmt.executeUpdate(createTable);
+        } catch (SQLException e) {
+            System.out.println("Table not created. " + e.getMessage());
+        }
+    }
+
+    private int getCount() {
+        int rows = 0;
+        String query = "SELECT COUNT(ID) FROM customer;";
+        try {
+            openConnection();
+            ResultSet rs = stmt.executeQuery(query);
+            rs.next();
+            rows = rs.getInt(1);
+        } catch (SQLException e) {
+            System.out.println("Something wrong with count method. " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Something wrong with count method. " + e.getMessage());
+        } finally {
+            closeConnection();
+        }
+        return rows;
+    }
+}
---
+++ Project-Costing/src/dao/DBHandler.java
@@ -0,0 +1,50 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author Marinus Toman
+ * Date: 17-Mar-2017
+ */
+public abstract class DBHandler {
+    // Instance Fields
+    protected Statement stmt;
+    protected Connection conn;
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://"
+        + "ec2-34-251-133-58.eu-west-1.compute.amazonaws.com:3306" 
+        + "/project_planner?autoReconnect=true&useSSL=false";
+    private static String username;
+    private static String password;
+    
+    // Constructors
+    protected DBHandler(String username, String password){
+        DBHandler.username = username;
+        DBHandler.password = password;
+    }
+    // Methods
+    protected void openConnection(){
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, username, password);
+            stmt = conn.createStatement();
+        } catch (SQLException e) {
+            System.out.println("Problem with SQL. " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("Problem with driver. " + e.getMessage());
+        }
+    }
+    
+    protected void closeConnection(){
+        if(conn != null){
+            try{
+                conn.close();
+            }catch(SQLException e){
+                System.out.println("Connection not closed. " + e.getMessage());
+            }
+        }
+    }
+}
