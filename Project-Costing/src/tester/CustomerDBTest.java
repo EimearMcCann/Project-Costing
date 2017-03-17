bc2d609b815a9c9405812d022e9bc613e68f179e
Marinus <marinus@home>
2017-03-17 23:00:09 +0000

    Created CustomerDBTest class.

---
+++ Project-Costing/src/tester/CustomerDBTest.java
@@ -0,0 +1,24 @@
+package tester;
+
+import classes.Customer;
+import dao.CustomerDB;
+
+/**
+ * @author Marinus Toman
+ *  Date: 17-Mar-2017
+ */
+public class CustomerDBTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Customer c = new Customer();
+        CustomerDB db = new CustomerDB("remoteuser", "password");
+        
+        c.read();
+        db.write(c);
+        System.out.print("Success, Written to DB");
+    }
+
+}
