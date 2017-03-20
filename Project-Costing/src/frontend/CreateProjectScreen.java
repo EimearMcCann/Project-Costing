7da50c0d1ef8ae691161129087673857f86933c3
Marinus <marinus@home>
2017-03-20 12:48:03 +0000

    Updated Project screen

--- Project-Costing/src/frontend/CreateProjectScreen.java
+++ Project-Costing/src/frontend/CreateProjectScreen.java
@@ -264,6 +264,7 @@ public class CreateProjectScreen extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new CreateProjectScreen().setVisible(true);
             }
