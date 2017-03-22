071e99b4be49e35bc27bf8e6550189fe161f5ba4
Marinus <marinus@home>
2017-03-22 11:18:23 +0000

    Updated Login screen, removed unused import statements.

--- Project-Costing/src/frontend/LoginScreen.java
+++ Project-Costing/src/frontend/LoginScreen.java
@@ -6,12 +6,9 @@
 package frontend;
 
 import java.awt.Color;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
 import javax.swing.ImageIcon;
-import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
 /**
