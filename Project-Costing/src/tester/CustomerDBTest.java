90316dba3e5d64d0f8f040aaabfeb368eda24fc3
Marinus <marinus@home>
2017-03-24 19:04:38 +0000

    Updated tester to test delete method.

--- Project-Costing/src/tester/CustomerDBTest.java
+++ Project-Costing/src/tester/CustomerDBTest.java
@@ -12,22 +12,24 @@ public class CustomerDBTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Customer c = new Customer();
+//        Customer c = new Customer();
         CustomerDB db = new CustomerDB("remoteuser", "password");
-
-        c.read();
-        db.write(c);
-        System.out.println("Success, Written to DB");
+//
+//        c.read();
+//        db.write(c);
+//        System.out.println("Success, Written to DB");
+//        
+//        Customer c1 = new Customer();
+//        c1.read();
+//        db.update(c1, 6);
+//
+//        Customer c2 = db.read(6);
+//        System.out.println("Reading from DB");
+//        if (c2 != null) {
+//            System.out.println(c2.toString());
+//        }
         
-        Customer c1 = new Customer();
-        c1.read();
-        db.update(c1, 6);
-
-        Customer c2 = db.read(6);
-        System.out.println("Reading from DB");
-        if (c2 != null) {
-            System.out.println(c2.toString());
-        }
+        db.delete(6);
 
     }
 
