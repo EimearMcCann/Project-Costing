c02cd554400da4cfcf3233af6852c0de1382feb0
Marinus <marinus@home>
2017-03-24 17:02:30 +0000

    Updated to include update method test.

--- Project-Costing/src/tester/CustomerDBTest.java
+++ Project-Costing/src/tester/CustomerDBTest.java
@@ -4,8 +4,7 @@ import classes.Customer;
 import dao.CustomerDB;
 
 /**
- * @author Marinus Toman
- *  Date: 17-Mar-2017
+ * @author Marinus Toman Date: 17-Mar-2017
  */
 public class CustomerDBTest {
 
@@ -15,10 +14,21 @@ public class CustomerDBTest {
     public static void main(String[] args) {
         Customer c = new Customer();
         CustomerDB db = new CustomerDB("remoteuser", "password");
-        
+
         c.read();
         db.write(c);
         System.out.println("Success, Written to DB");
+        
+        Customer c1 = new Customer();
+        c1.read();
+        db.update(c1, 6);
+
+        Customer c2 = db.read(6);
+        System.out.println("Reading from DB");
+        if (c2 != null) {
+            System.out.println(c2.toString());
+        }
+
     }
 
 }
